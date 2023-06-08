a0d84567043d2f615b27ed702b31cba191d9df2a
Jonathan Eduardo <jonnymartinez.pzl1@gmail.com>
2023-06-08 10:35:43 -0600

    Seguimiento a las anotaciones @PathVariable, @RequestParam

--- src/main/java/com/springboot/web/controllers/ParametrosController.java
+++ src/main/java/com/springboot/web/controllers/ParametrosController.java
@@ -6,13 +6,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/params")
 public class ParametrosController {
 
+    @GetMapping("/")
+    public String index(){
+        return "params/index";
+    }
+
     @GetMapping("/string")
     public String param(@RequestParam(name = "texto", required = false, defaultValue = "Sin parametros") String texto, Model model){
         model.addAttribute("resultado", "El texto enviado es: "+ texto);
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params")
+    public String param(@RequestParam String saludo,
+                        @RequestParam Integer numero,
+                        Model model){
+        model.addAttribute("resultado", "El saludo enviado es: "+ saludo + " y el número es: " + numero);
+        return "params/ver";
+    }
+
+    @GetMapping("/mix-params-request")
+    public String param(HttpServletRequest request, Model model){
+        String saludo = request.getParameter("saludo");
+        Integer numero = 0;
+        try{
+            numero = Integer.parseInt(request.getParameter("numero"));
+        } catch (NumberFormatException e){
+            //mensaje de error
+        }
+        model.addAttribute("resultado", "El saludo enviado es: "+ saludo + " y el número es: " + numero);
         return "params/ver";
     }
 }
